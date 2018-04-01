0bcc9af052809a3cf95a3bb91402cbfd55d8e088
mrdShinse <shinse1128@hotmail.com>
2018-04-01 21:10:27 +0900

    display your maps.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@
 class HomeController < ApplicationController # :nodoc:
   def index
     @maps = Map.order(id: :desc).limit(20)
+    @yours = current_user.maps.order(id: :desc).limit(20) if user_signed_in?
   end
 end
