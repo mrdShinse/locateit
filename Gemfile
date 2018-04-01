2b4149c1f014cc57f2a110b71f82bc0fd3550493
mrdShinse <shinse1128@hotmail.com>
2018-04-01 15:18:04 +0900

    install devise.

--- Gemfile
+++ Gemfile
@@ -29,6 +29,9 @@ gem 'uglifier', '>= 1.3.0'
 # frontend
 gem 'webpacker'
 
+# auth
+gem 'devise'
+
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::Base # :nodoc:
+  protect_from_forgery with: :exception, prepend: true
+  before_action :authenticate_user!
 end
